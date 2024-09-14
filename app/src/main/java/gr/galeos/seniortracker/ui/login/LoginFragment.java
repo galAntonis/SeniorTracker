5c887ff2f741d1fffc6b77ac573c36cb458bcd98
galAntonis <galant.dev@gmail.com>
2024-09-14 16:50:30 +0300

    Created the ui for login and signup fragments
    Signed-off-by: galAntonis <galant.dev@gmail.com>

---
+++ app/src/main/java/gr/galeos/seniortracker/ui/login/LoginFragment.java
@@ -0,0 +1,36 @@
+package gr.galeos.seniortracker.ui.login;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import gr.galeos.seniortracker.R;
+
+public class LoginFragment extends Fragment {
+
+    private LoginViewModel mViewModel;
+
+    public static LoginFragment newInstance() {
+        return new LoginFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/gr/galeos/seniortracker/ui/signup/SignUpViewModel.java
@@ -0,0 +1,7 @@
+package gr.galeos.seniortracker.ui.signup;
+
+import androidx.lifecycle.ViewModel;
+
+public class SignUpViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
